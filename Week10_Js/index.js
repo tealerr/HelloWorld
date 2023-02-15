7cd995c7bfd40c09314b2802ba153b72ba898c19
tealerr <theetitle@gmail.com>
2023-02-15 18:36:16 +0700

    homework drum kit

---
+++ Week10_Js/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").innerHTML = "Hello World!!!"
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild.innerHTML = "Hello PIM"
+
+document.querySelector("h1").style.color = "red"
