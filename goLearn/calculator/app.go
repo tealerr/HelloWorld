366fb61f22b3b23da47c98a58169fa871576ef17
tealerr <theetitle@gmail.com>
2023-06-14 09:35:19 +0700

    add struct

---
+++ goLearn/calculator/app.go
@@ -0,0 +1,9 @@
+package calculator
+
+func Add(number1, number2 int) int { //Add พิมใหญ่เพื่อให้เป็น public
+
+	return number1 + number2
+}
+func Subtract(number1, number2 int) int {
+	return number1 - number2
+}
--- goLearn/program.go
+++ goLearn/program.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"Go/calculator"
+	"fmt"
+)
 
-//นิยามตัวแปร
+// นิยามตัวแปร
 func main() {
 	var num = []int{100, 200, 300}
 	name := []string{"A", "B", "C", "D", "E", "F", "G"}
 	name = append(name, "C", "d", "f") //เพิ่มค่าเข้า array name โดยเพิ่ม C,d,f
-	num = append(num, 400, 500, 600)
+	num = append(num, 400, 500, 600)   // arr name = append(name, ค่าที่ต้องการเพิ่ม)
 
 	fmt.Println(num[2:5]) //เริ่ม index 2 และจบที่ index4
 	fmt.Println(name[1:])
@@ -73,15 +76,27 @@ func main() {
 	myCart := getTotalCart(500, 500) //ถ้า func มีการ Return ต้องสร้างตัวแปรขึ้นมารับค่า
 	fmt.Println("ยอดรวมสินค้า ", myCart)
 
-	result, checked := summation(5, 200)
-	fmt.Println(result, checked)
+	result := summation(5, 200, 300, 400, 123, 525, 1231)
+	fmt.Println("ผลรวม", result)
 
+	product1 := product{name: "Pen", price: 30, category: "เครื่องเขียน", discount: 10}
+	product2 := product{name: "mouse", price: 900, category: "อุปกรณ์คอมพิวเตอร์", discount: 10}
+	product3 := product{name: "keyboard", price: 500, category: "อุปกรณ์คอมพิวเตอร์", discount: 10}
+	product4 := product{name: "monitor", price: 2000, category: "อุปกรณ์คอมพิวเตอร์", discount: 10}
+	product5 := product{name: "หูฟัง", price: 300, category: "อุปกรณ์คอมพิวเตอร์", discount: 30}
+	product1.price = 100
+	product5.name, product2.name = "ดินสอ", "ยางลบ"
+
+	fmt.Println(product1.name, product2.name, product3.name, product4.name, product5.name)
+	fmt.Println(calculator.Add(50, 100))
+	fmt.Println(calculator.Subtract(100, 50))
+	testMap()
 }
 
 func showMessage(name string) {
 	fmt.Println("Hello ", name)
 }
-func total(x, y int) { //func รับค่า
+func total(x, y int) { //func รับค่าwww
 	//argument ค่าที่ส่งเข้าฟังก์ชัน 10,5 เป็น argument ที่ส่งเข้ามาใน parameter x,y
 	fmt.Println("ยอดรวม ", x+y)
 }
@@ -92,15 +107,27 @@ func getTotalCart(x, y int) int { //func รับค่าและ return
 	total := x + y
 	return total
 }
-func summation(x, y int) (int, string) {
-	total := x + y
-	status := ""
-
-	if total%2 == 0 {
-		status = "เลขคู่"
-	} else {
-		status = "เลขคี่"
+func summation(x ...int) int { // variadic function รับค่าได้ไม่จำกัด
+	total := 0
+	for _, value := range x {
+		total += value
 	}
+	return total
+}
 
-	return total, status
+// struct เก็บข้อมูลที่ต่าง type กัน ต่างจาก array ที่ต้องเก็บเฉพาะ type เดียวกัน
+
+// struct เก็บข้อมูลสินค้า
+type product struct {
+	name     string
+	price    float32
+	category string
+	discount int
+}
+
+func testMap() {
+	coin := map[string]string{"BTC": "bitcoin", "ETH": "Ether"}
+	for key, value := range coin {
+		println("Key= ", key, "value= ", value)
+	}
 }
